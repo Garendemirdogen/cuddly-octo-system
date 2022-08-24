c41224c90fb99470cabc7451e644e2010dbdb52f
Garen <garendemirdogen1@gmail.com>
2022-08-23 23:33:14 -0400

    add app.use, added app.get

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ var fs = require("fs");
 // Express app start
 var app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Express app to handle data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("assets"));
+app.use(express.static("public"));
+
+var note = [];
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
