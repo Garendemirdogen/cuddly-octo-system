b4d4a113ce9eb198cad22d45e1edd96ac5b93c18
Garen <garendemirdogen1@gmail.com>
2022-08-25 21:42:24 -0400

    changed express variable to more relatable name

--- server.js
+++ server.js
@@ -3,35 +3,35 @@ var path = require("path");
 var fs = require("fs");
 
 // Express app start
-var app = express();
+var expressApp = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Express app to handle data and parsing of data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+expressApp.use(express.urlencoded({ extended: true }));
+expressApp.use(express.json());
+expressApp.use(express.static("public"));
 
 var note = [];
 
-app.get("/", (req, res) => {
+expressApp.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", (req, res) => {
+expressApp.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 // To display all notes that have been made
-app.get("/api/notes", (req, res) => {
+expressApp.get("/api/notes", (req, res) => {
   return res.json(note);
 });
 
-app.post("/api/notes", (req, res) => {
+expressApp.post("/api/notes", (req, res) => {
   var newNotes = req.body;
   note.push(newNotes);
   res.json(newNotes);
 });
 
 // Starts the server
-app.listen(PORT, () => {
+expressApp.listen(PORT, () => {
   console.log(`API server is now listening on port ${PORT}!`);
 });
